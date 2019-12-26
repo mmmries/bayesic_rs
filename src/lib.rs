5475406e9092c998c686c8e0bab58da8fae969ec
mmmries <riesmmm@gmail.com>
2019-12-26 13:33:52 -0700

    get the basics of classification working

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,52 @@
 use std::collections::{HashMap,HashSet};
-//use std::collections::hash_map::Keys;
 
-struct Trainer {
+pub struct Bayesic {
+  classifications: HashSet<String>,
   by_token: HashMap<String, HashSet<String>>,
-  by_class: HashMap<String, HashSet<String>>,
+}
+
+impl Bayesic {
+  pub fn new() -> Bayesic {
+    Bayesic {
+      classifications: HashSet::new(),
+      by_token: HashMap::new(),
+    }
+  }
+
+  pub fn classify(& self, tokens: Vec<String>) -> HashMap<String, f64> {
+    let mut probabilities = HashMap::new();
+    let num_classes = self.classifications.len() as f64;
+    let prior: f64 = 1.0 / num_classes;
+    for class in &self.classifications {
+      let mut p_klass = prior;
+      for token in &tokens {
+        if self.by_token[token].contains(class) {
+          let p_not_klass: f64 = 1.0 - p_klass;
+          let p_token_given_klass: f64 = 1.0;
+          let num_potential_classes: f64 = self.by_token[token].len() as f64;
+          let p_token_given_not_klass: f64 = (num_potential_classes - 1.0) / num_classes;
+          p_klass = (p_token_given_klass * p_klass) / ((p_token_given_klass * p_klass) + (p_token_given_not_klass * p_not_klass));
+        }
+      }
+      probabilities.insert(class.clone(), p_klass);
+    }
+    return probabilities;
+  }
+
+  pub fn train(&mut self, class: String, tokens: Vec<String>) {
+    self.classifications.insert(class.clone());
+    for token in tokens {
+      self.by_token.entry(token).or_insert(HashSet::new()).insert(class.clone());
+    }
+  }
 }
 
 #[cfg(test)]
 mod tests {
   extern crate pretty_assertions;
 
-  use std::path::{Path,PathBuf};
+  use super::*;
+  use std::path::Path;
   use lazy_static::lazy_static;
   use regex::Regex;
 
@@ -18,17 +54,68 @@ mod tests {
     static ref WORDS: Regex = Regex::new(r"\b\w+\b").unwrap();
   }
 
-  fn path_to_words(path: &PathBuf) -> Vec<String> {
+  lazy_static! {
+    static ref TRAINED: Bayesic = trained();
+  }
+
+  fn path_to_words(path_str: &str) -> Vec<String> {
+    let path = Path::new(path_str).to_path_buf();
     let bin = std::fs::read(path).ok().unwrap();
     let s = String::from_utf8(bin).ok().unwrap();
     let words: Vec<String> = WORDS.find_iter(&s).map(|m: regex::Match| String::from(m.as_str()) ).collect();
     return words;
   }
 
+  fn trained() -> Bayesic {
+    let mut bayesic = Bayesic::new();
+    bayesic.train("jojo".to_string(), path_to_words("priv/training/jojo_rabbit"));
+    bayesic.train("jurassic_park".to_string(), path_to_words("priv/training/jurassic_park"));
+    bayesic.train("jurassic_park_ii".to_string(), path_to_words("priv/training/jurassic_park_ii"));
+    bayesic.train("jurassic_park_iii".to_string(), path_to_words("priv/training/jurassic_park_iii"));
+    bayesic.train("kpax".to_string(), path_to_words("priv/training/kpax"));
+    return bayesic;
+  }
+
   #[test]
   fn parsing_file_words() {
     let strs = vec!("A", "young", "boy", "in", "Hitler", "s", "army", "finds", "out", "his", "mother", "is", "hiding", "a", "Jewish", "girl", "in", "their", "home");
     let expected: Vec<String> = strs.iter().map(|s| s.to_string()).collect();
-    assert_eq!(path_to_words(&Path::new("priv/training/jojo_rabbit").to_path_buf()), expected)
+    assert_eq!(path_to_words("priv/training/jojo_rabbit"), expected)
+  }
+
+  #[test]
+  fn empty_classification() {
+    let classification = TRAINED.classify(vec!());
+    assert_eq!(classification["jojo"], 0.2);
+  }
+
+  #[test]
+  fn key_word_classification() {
+    let classification = TRAINED.classify(vec!("Hitler".to_string()));
+    assert!(classification["jojo"] > 0.9);
+    assert!(classification["jurassic_park"] < 0.25);
+  }
+
+  #[test]
+  fn generic_word_classification() {
+    let classification = TRAINED.classify(vec!("a".to_string()));
+    // three of our movie descriptions have "a", so it's not as strong of an indicator
+    assert!(classification["jojo"] > 0.35);
+    assert!(classification["jurassic_park"] > 0.35);
+    assert!(classification["kpax"] > 0.35);
+    // two of our movies don't have it so they should just have the prior probability
+    assert_eq!(classification["jurassic_park_ii"], 0.2);
+    assert_eq!(classification["jurassic_park_iii"], 0.2);
+  }
+
+  #[test]
+  fn multiple_generics() {
+    let classification = TRAINED.classify(vec!("a".to_string(), "the".to_string(), "with".to_string()));
+    // only jurassic_park has all of these fairly generic words
+    assert!(classification["jurassic_park"] > 0.75);
+    assert!(classification["jurassic_park"] > classification["jurassic_park_ii"]);
+    // Other movies have just one of these words so they only get a small boost to probability
+    assert!(classification["kpax"] < 0.4);
+    assert!(classification["jojo"] < 0.4);
   }
 }
