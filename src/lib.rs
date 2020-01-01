bdd54e382be39957c07a02a078ca53d40f3cbdc6
mmmries <riesmmm@gmail.com>
2019-12-31 21:48:41 -0700

    only expect classifications when probability is greater than the prior

--- src/lib.rs
+++ src/lib.rs
@@ -16,19 +16,20 @@ impl Bayesic {
   pub fn classify(& self, tokens: Vec<String>) -> HashMap<String, f64> {
     let mut probabilities = HashMap::new();
     let num_classes = self.classifications.len() as f64;
-    let prior: f64 = 1.0 / num_classes;
-    for class in &self.classifications {
-      let mut p_klass = prior;
-      for token in &tokens {
-        if self.by_token.contains_key(token) && self.by_token[token].contains(class) {
+    for token in &tokens {
+      if self.by_token.contains_key(token) {
+        let possible_classes = self.by_token.get(token).unwrap();
+        for class in possible_classes {
+          let prior = 1.0 / num_classes;
+          let mut p_klass: f64 = *probabilities.get(class).unwrap_or(&prior);
           let p_not_klass: f64 = 1.0 - p_klass;
           let p_token_given_klass: f64 = 1.0;
           let num_potential_classes: f64 = self.by_token[token].len() as f64;
           let p_token_given_not_klass: f64 = (num_potential_classes - 1.0) / num_classes;
           p_klass = (p_token_given_klass * p_klass) / ((p_token_given_klass * p_klass) + (p_token_given_not_klass * p_not_klass));
+          probabilities.insert(class.clone(), p_klass);
         }
       }
-      probabilities.insert(class.clone(), p_klass);
     }
     return probabilities;
   }
@@ -86,26 +87,24 @@ mod tests {
   #[test]
   fn empty_classification() {
     let classification = TRAINED.classify(vec!());
-    assert_eq!(classification["jojo"], 0.2);
+    assert_eq!(classification.len(), 0);
   }
 
   #[test]
   fn key_word_classification() {
     let classification = TRAINED.classify(vec!("Hitler".to_string()));
     assert!(classification["jojo"] > 0.9);
-    assert!(classification["jurassic_park"] < 0.25);
+    assert_eq!(classification.len(), 1);
   }
 
   #[test]
   fn generic_word_classification() {
     let classification = TRAINED.classify(vec!("a".to_string()));
     // three of our movie descriptions have "a", so it's not as strong of an indicator
+    assert_eq!(classification.len(), 3);
     assert!(classification["jojo"] > 0.35);
     assert!(classification["jurassic_park"] > 0.35);
     assert!(classification["kpax"] > 0.35);
-    // two of our movies don't have it so they should just have the prior probability
-    assert_eq!(classification["jurassic_park_ii"], 0.2);
-    assert_eq!(classification["jurassic_park_iii"], 0.2);
   }
 
   #[test]
@@ -117,5 +116,11 @@ mod tests {
     // Other movies have just one of these words so they only get a small boost to probability
     assert!(classification["kpax"] < 0.4);
     assert!(classification["jojo"] < 0.4);
+  }
+
+  #[test]
+  fn un_indexed_tokens() {
+    let classification = TRAINED.classify(vec!("zzz".to_string(), "yyy".to_string()));
+    assert_eq!(classification.keys().len(), 0);
   }
 }
